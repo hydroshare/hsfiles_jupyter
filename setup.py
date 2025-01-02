769eec4cbde273ea62a0b2275b961984a7347b92
pkdash <pabitra.dash@usu.edu>
2025-01-02 15:15:10 -0500

    making the jupyter related python module dependencies to match with 2i2c

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'notebook',
-        'hsclient'
+        'hsclient>=1.0.5',
+        'notebook==6.4.*',
+        'jupyterlab==4.1.*',
+        'jupyter_server==2.13.*',
     ],
 )
